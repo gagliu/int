9ec1516a91ac9f6a401af307a16039fae2025780
luismec90 <luismec90@gmail.com>
2014-03-01 16:36:57 -0500

    Mejoras

--- application/config/config.php
+++ application/config/config.php
@@ -14,7 +14,7 @@
 | path to your installation.
 |
 */
-$config['base_url']	= 'http://localhost/codeigniter_template/';
+$config['base_url']	= 'http://localhost/tropic/';
 
 /*
 |--------------------------------------------------------------------------
