54c664e8fdaed47a31ff7fd6b6e93473683eb527
Juan Diego <juandi70@hotmail.com>
2014-03-01 19:57:12 -0500

    Merge origin/master
    Conflicts:
    	application/views/include/header.php

--- application/views/inicio_view.php
+++ application/views/inicio_view.php
@@ -9,9 +9,9 @@
                     <h3>Internacionalización Agroindustrial</h3>
                     <hr class="intro-divider">
                     <ul class="list-inline intro-social-buttons">
-                        <li><a href="https://twitter.com/SBootstrap" class="btn btn-default btn-lg"><i class="fa fa-twitter fa-fw"></i> <span class="network-name">Twitter</span></a>
+                        <li><a href="https://twitter.com/" class="btn btn-default btn-lg"><i class="fa fa-twitter fa-fw"></i> <span class="network-name">Twitter</span></a>
                         </li>
-                        <li><a href="https://github.com/IronSummitMedia/startbootstrap" class="btn btn-default btn-lg"><i class="fa fa-github fa-fw"></i> <span class="network-name">Github</span></a>
+                        <li><a href="https://www.facebook.com/tropico.internacional?fref=ts" class="btn btn-default btn-lg"><i class="fa fa-facebook fa-fw"></i> <span class="network-name">Facebook</span></a>
                         </li>
                         <li><a href="#" class="btn btn-default btn-lg"><i class="fa fa-linkedin fa-fw"></i> <span class="network-name">Linkedin</span></a>
                         </li>
@@ -34,18 +34,18 @@
                 <img class="img-circle" data-src="holder.js/140x140" alt="140x140" style="width: 140px; height: 140px;" src="<?= base_url() ?>assets/img/compet.jpg">
                 <h2>Sostenibilidad</h2>
                 <p>Gestión sostenible de recursos naturales<br> en la producción de alimentos.<br>Aire limpio + Agua pura + Alimentos confiables</p>
-                <p><a class="btn btn-default" href="#" role="button">View details »</a></p>
+                
             </div><!-- /.col-lg-4 -->
             <div class="col-lg-4 text-center">
                 <img class="img-circle" data-src="holder.js/140x140" alt="140x140" style="width: 140px; height: 140px;" src="<?= base_url() ?>assets/img/farm.jpg">
                 <h2>Competitividad</h2>
                 <p>A través de la EPSAGRO  Tropico Internacional, apoyamos el sector de producción primaria con enfoque agroexportador y de inserción internacional.</p>
-                <p><a class="btn btn-default" href="#" role="button">View details »</a></p>
+                
             </div><!-- /.col-lg-4 -->
             <div class="col-lg-4 text-center">
                 <img class="img-circle" data-src="holder.js/140x140" alt="140x140" style="width: 140px; height: 140px;" src="<?= base_url() ?>assets/img/mundo.jpg">
                 <h2>Globalización</h2>
-                <p>Información mundial en un click:<br> Actualidad nacional e internacional.<br>Tendencias globales de consumo.</p><p><a class="btn btn-default" href="#" role="button">View details »</a></p>
+                <p>Información mundial en un click:<br> Actualidad nacional e internacional.<br>Tendencias globales de consumo.</p>
             </div><!-- /.col-lg-4 -->
         </div>
 
@@ -63,7 +63,7 @@
                 <div class="clearfix"></div>
                 <h2 class="section-heading">Facilitación del Comercio:
                     <br>Tropic e-Market Tool</h2>
-                <p class="lead">A special thanks to Death to the Stock Photo for providing the photographs that you see in this template. <a target="_blank" href="http://join.deathtothestockphoto.com/">Visit their website</a> to become a member.</p>
+                <p class="lead">Contacte compradores en cualquier lugar del mundo.</p>
             </div>
             <div class="col-lg-5 col-lg-offset-2 col-sm-6">
                 <img class="img-responsive" src="<?= base_url() ?>assets/img/ipad.png" alt="">
@@ -84,9 +84,9 @@
             <div class="col-lg-5 col-lg-offset-1 col-sm-push-6  col-sm-6">
                 <hr class="section-heading-spacer">
                 <div class="clearfix"></div>
-                <h2 class="section-heading">3D Device Mockups
-                    <br>by PSDCovers</h2>
-                <p class="lead">Turn your 2D designs into high quality, 3D product shots in seconds using free Photoshop actions by PSDCovers! <a target="_blank" href="http://www.psdcovers.com/">Visit their website</a> to download some of their awesome, free photoshop actions!</p>
+                <h2 class="section-heading">Asociatividad y 
+                    <br>Cooperativismo</h2>
+                <p class="lead"></p>
             </div>
             <div class="col-lg-5 col-sm-pull-6  col-sm-6">
                 <img class="img-responsive" src="<?= base_url() ?>assets/img/doge.png" alt="">
@@ -107,9 +107,9 @@
             <div class="col-lg-5 col-sm-6">
                 <hr class="section-heading-spacer">
                 <div class="clearfix"></div>
-                <h2 class="section-heading">Google Web Fonts and
-                    <br>Font Awesome Icons</h2>
-                <p class="lead">This template features the 'Lato' font, part of the <a target="_blank" href="http://www.google.com/fonts">Google Web Font library</a>, as well as <a target="_blank" href="http://fontawesome.io">icons from Font Awesome</a>.</p>
+                <h2 class="section-heading">Inclusion social y
+                    <br>educación rural</h2>
+                <p class="lead"></a>.</p>
             </div>
             <div class="col-lg-5 col-lg-offset-2 col-sm-6">
                 <img class="img-responsive" src="<?= base_url() ?>assets/img/phones.png" alt="">
@@ -126,7 +126,7 @@
         <div class="container-contact container">
             <div class="row">
                 <div class="col-sm-12">
-                    <h1 class="header-title">CONTACT US</h1>
+                    <h1 class="header-title">CONTACTENOS</h1>
                     <h2 class="subtitle">Feel free to contact us anytime !</h2>
                     <br>
                     <hr class="hr-contact">
@@ -200,9 +200,9 @@
             </div>
             <div class="col-lg-6">
                 <ul class="list-inline banner-social-buttons">
-                    <li><a href="https://twitter.com/SBootstrap" class="btn btn-default btn-lg"><i class="fa fa-twitter fa-fw"></i> <span class="network-name">Twitter</span></a>
+                    <li><a href="https://twitter.com/" class="btn btn-default btn-lg"><i class="fa fa-twitter fa-fw"></i> <span class="network-name">Twitter</span></a>
                     </li>
-                    <li><a href="https://github.com/IronSummitMedia/startbootstrap" class="btn btn-default btn-lg"><i class="fa fa-github fa-fw"></i> <span class="network-name">Github</span></a>
+                    <li><a href="https://www.facebook.com/tropico.internacional?fref=ts" class="btn btn-default btn-lg"><i class="fa fa-github fa-fw"></i> <span class="network-name">Github</span></a>
                     </li>
                     <li><a href="#" class="btn btn-default btn-lg"><i class="fa fa-linkedin fa-fw"></i> <span class="network-name">Linkedin</span></a>
                     </li>
